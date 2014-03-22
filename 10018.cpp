5d96c893f16b1a55aa84ec7af132095d703a1523
kaaarel <kaarel.randorg@gmail.com>
2014-03-23 01:04:56 +0200

    proob

---
+++ 10018.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <math.h>
+#include <limits>
+#include <inttypes.h>
+
+using namespace std;
+
+int poora(int arv){
+
+    int pooratud = 0;
+    int j = 0;
+
+
+    while(arv > 0){
+        j = arv%10;
+        pooratud = pooratud * 10 + j;
+        arv = arv/10;
+
+    }
+    return pooratud;
+
+}
+
+
+
+int main()
+{
+    int P,N,n=0,i;
+    cin>>N;
+    
+    for(i=1;i<=N;i++){
+    cin>>P;
+        while(P!=poora(P)){
+            P=P+poora(P);
+            n++;
+        }
+    cout<<n<<" "<<P<<endl;
+    n=0;
+    }
+    system("pause");
+    return 0;
+}
